72b3bb8310307d3841bf64c5cc38496cdb63ca24
AlexFlipnote <alexander.samuelsen.manso@gmail.com>
2017-10-31 20:00:46 +0100

    Added more perms

--- assets/permissions.py
+++ assets/permissions.py
@@ -3,3 +3,19 @@ import discord
 
 def can_send(ctx):
     return isinstance(ctx.channel, discord.DMChannel) or ctx.channel.permissions_for(ctx.guild.me).send_messages
+
+
+def can_embed(ctx):
+    return isinstance(ctx.channel, discord.DMChannel) or ctx.channel.permissions_for(ctx.guild.me).embed_links
+
+
+def can_upload(ctx):
+    return isinstance(ctx.channel, discord.DMChannel) or ctx.channel.permissions_for(ctx.guild.me).attach_files
+
+
+def can_react(ctx):
+    return isinstance(ctx.channel, discord.DMChannel) or ctx.channel.permissions_for(ctx.guild.me).add_reactions
+
+
+def is_nsfw(ctx):
+    return isinstance(ctx.channel, discord.DMChannel) or ctx.channel.is_nsfw()
