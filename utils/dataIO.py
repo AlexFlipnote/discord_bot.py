4b947f16daba0ec1edc65f1c68c49531c5ecbc03
AlexFlipnote <alexander.samuelsen.manso@gmail.com>
2018-05-22 21:08:24 +0200

    Added a new value if needed

--- utils/dataIO.py
+++ utils/dataIO.py
@@ -11,3 +11,15 @@ def change_value(file, value, changeto):
     data[value] = changeto
     with open(file, "w") as jsonFile:
         json.dump(data, jsonFile, indent=2)
+
+
+def append_value(file, value, addition):
+    try:
+        with open(file, "r") as jsonFile:
+            data = json.load(jsonFile)
+    except FileNotFoundError:
+        raise FileNotFoundError("The file you tried to get does not exist...")
+
+    data[value].append(addition)
+    with open(file, "w") as jsonFile:
+        json.dump(data, jsonFile, indent=2)
