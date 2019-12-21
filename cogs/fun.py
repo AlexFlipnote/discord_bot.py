bb620c923c26a14845aef3c1dd1f8a955f5336b2
AlexFlipnote <alexander.samuelsen.manso@gmail.com>
2019-12-21 14:52:51 +0100

    Added beer command as example of reaction wait_for

--- cogs/fun.py
+++ cogs/fun.py
@@ -2,6 +2,7 @@ import random
 import discord
 import urllib
 import secrets
+import asyncio
 import aiohttp
 import re
 
@@ -192,6 +193,38 @@ class Fun_Commands(commands.Cog):
             deci = 0
 
         await ctx.send(f"I'd rate {thing} a **{num}.{deci} / 100**")
+
+    @commands.command()
+    async def beer(self, ctx, user: discord.Member = None, *, reason: commands.clean_content = ""):
+        """ Give someone a beer! 🍻 """
+        if not user or user.id == ctx.author.id:
+            return await ctx.send(f"**{ctx.author.name}**: paaaarty!🎉🍺")
+        if user.id == self.bot.user.id:
+            return await ctx.send("*drinks beer with you* 🍻")
+        if user.bot:
+            return await ctx.send(f"I would love to give beer to the bot **{ctx.author.name}**, but I don't think it will respond to you :/")
+
+        beer_offer = f"**{user.name}**, you got a 🍺 offer from **{ctx.author.name}**"
+        beer_offer = beer_offer + f"\n\n**Reason:** {reason}" if reason else beer_offer
+        msg = await ctx.send(beer_offer)
+
+        def reaction_check(m):
+            if (m.message_id == msg.id and m.user_id == user.id and str(m.emoji) == "🍻"):
+                return True
+            return False
+
+        try:
+            await msg.add_reaction("🍻")
+            await self.bot.wait_for('raw_reaction_add', timeout=30.0, check=reaction_check)
+            await msg.edit(content=f"**{user.name}** and **{ctx.author.name}** are enjoying a lovely beer together 🍻")
+        except asyncio.TimeoutError:
+            await msg.delete()
+            await ctx.send(f"well, doesn't seem like **{user.name}** wanted a beer with you **{ctx.author.name}** ;-;")
+        except discord.Forbidden:
+            # Yeah so, bot doesn't have reaction permission, drop the "offer" word
+            beer_offer = f"**{user.name}**, you got a 🍺 from **{ctx.author.name}**"
+            beer_offer = beer_offer + f"\n\n**Reason:** {reason}" if reason else beer_offer
+            await msg.edit(content=beer_offer)
 
     @commands.command(aliases=['howhot', 'hot'])
     async def hotcalc(self, ctx, *, user: discord.Member = None):
