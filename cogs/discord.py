1ade3cf51ca77c5d68980db69599cbfc14e9f221
KILR <65610641+KILR007@users.noreply.github.com>
2020-08-31 12:22:53 +0530

    Made join_position a string

--- cogs/discord.py
+++ cogs/discord.py
@@ -125,7 +125,7 @@ class Discord_Info(commands.Cog):
 
         embed.add_field(name="Full name", value=user, inline=True)
         embed.add_field(name="Nickname", value=user.nick if hasattr(user, "nick") else "None", inline=True)
-        embed.add_field(name="Join position",value=join_position,inline=True)
+        embed.add_field(name="Join position",value=str(join_position),inline=True)
         embed.add_field(name="Account created", value=default.date(user.created_at), inline=True)
         embed.add_field(name="Joined this server", value=default.date(user.joined_at), inline=True)
         embed.add_field(
