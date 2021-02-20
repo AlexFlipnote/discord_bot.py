3767d198a4ca04191770d60d14899cef42988a74
ecorte <antoine.turgeon2005@hotmail.com>
2021-02-20 14:06:41 -0500

    Server config v.2 (not working)

--- cogs/config.py
+++ cogs/config.py
@@ -6,8 +6,9 @@ import os
 import sys
 import json
 
+from io import BytesIO
 from discord.ext import commands
-from utils import permissions, default, http
+from utils import lists, permissions, http, default, argparser
 
 server_id = cxt.guild.id
 
@@ -17,3 +18,18 @@ with open(f'ss_{server_id}.json', 'r') as server_config:
 
 custom_server_prefix = str(obj['prefix'])
 mod_log_channel = str(obj['mod_log_channel'])
+
+
+class Server_Config(commands.Cog):
+    def __init__(self, bot):
+        self.bot = bot
+        self.config = default.config()
+
+    @commands.command()
+    async def testmodlogs(self):
+        channel = client.get_channel(mod_log_channel)
+        await channel.send("ModLog test")
+
+
+def setup(bot):
+    bot.add_cog(Server_Config(bot))
