b9ce00aae515b29823b8ae164850a8f15ac4eee0
elf <49889930+elfq@users.noreply.github.com>
2021-02-22 16:04:30 -0500

    allowedmentions (#47)
    Sure

--- index.py
+++ index.py
@@ -10,6 +10,7 @@ print("Logging in...")
 bot = Bot(
     command_prefix=config["prefix"], prefix=config["prefix"],
     owner_ids=config["owners"], command_attrs=dict(hidden=True), help_command=HelpFormat(),
+    allowed_mentions=discord.AllowedMentions(roles=False, users=True, everyone=False),
     intents=discord.Intents(  # kwargs found at https://discordpy.readthedocs.io/en/latest/api.html?highlight=intents#discord.Intents
         guilds=True, members=True, messages=True, reactions=True, presences=True
     )
