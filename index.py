cc63f08b6357c12b78bb493a907e68f92227da40
AlexFlipnote <alexander.samuelsen.manso@gmail.com>
2022-02-20 13:26:51 +0100

    Comply with API v10

--- index.py
+++ index.py
@@ -6,13 +6,14 @@ from utils.data import Bot, HelpFormat
 
 config = default.config()
 print("Logging in...")
+print(f"Discord API v{discord.http.API_VERSION}")
 
 bot = Bot(
     command_prefix=config["prefix"], prefix=config["prefix"],
     owner_ids=config["owners"], command_attrs=dict(hidden=True), help_command=HelpFormat(),
     allowed_mentions=discord.AllowedMentions(roles=False, users=True, everyone=False),
-    intents=discord.Intents(  # kwargs found at https://discordpy.readthedocs.io/en/latest/api.html?highlight=intents#discord.Intents
-        guilds=True, members=True, messages=True, reactions=True, presences=True
+    intents=discord.Intents(  # kwargs found at https://docs.pycord.dev/en/master/api.html?highlight=discord%20intents#discord.Intents
+        guilds=True, members=True, messages=True, reactions=True, presences=True, message_content=True,
     )
 )
 
